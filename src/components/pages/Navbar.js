61617ceef28831ebb1d3c04d6c9db10d18e63b24
Humayun Javed <arnetraff@gmail.com>
2021-03-24 20:13:52 +0500

    Changes

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import { AiOutlineInfo } from 'react-icons/ai';
-import styles from '../styles/Navbar.module.css';
+import React from "react";
+import { BsInfo } from "react-icons/bs";
+import styles from "../styles/Navbar.module.css";
 const Navbar = () => {
-	return (
-		<div className={styles.container}>
-			<div>
-				<h1>DW</h1>
-			</div>
-			<div className={styles.m_nav}>
-				<p>WATCHES</p>
-				<p>ABOUT</p>
-			</div>
-			<div className={styles.info}>
-				<p>
-					<AiOutlineInfo />
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <h1 style={{fontFamily:'serif',fontSize:'20pt',fontWeight:'300'}}>DW</h1>
+      <div className={styles.m_nav}>
+        <p>WATCHES</p>
+        <p>ABOUT</p>
+      </div>
+      <div className={styles.info}>
+        <p>
+          <BsInfo />
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
